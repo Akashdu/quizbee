7315131b83ee1e588ca0d71653dcecb03e72e92b
Akashdu <47060506+Akashdu@users.noreply.github.com>
2020-05-02 18:34:53 +0530

    Initial commit

---
+++ app/src/main/java/com/akash/quizbee/DeveloperActivity.java
@@ -0,0 +1,31 @@
+package com.akash.quizbee;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.vikasojha.quizbee.R;
+
+public class DeveloperActivity extends AppCompatActivity {
+    Button btnRestart;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_developer);
+
+        btnRestart = (Button) findViewById(R.id.button4);
+
+
+        btnRestart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in2 = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(in2);
+            }
+        });
+    }
+}
